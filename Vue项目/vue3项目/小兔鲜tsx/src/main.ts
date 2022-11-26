82b81d5035ac8eab73ba4b34d0b7170020a59e0f
1959377950 <1959377950@qq.com>
2022-11-26 12:37:26 +0800

    vue

--- Vue项目/vue3项目/小兔鲜tsx/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import App from './App'
-import router from './router'
-import store from './store'
-import 'normalize.css'
-import '@/assets/styles/common.less'
-import Ui from '@/utils/index'
-createApp(App).use(store).use(router).use(Ui).mount('#app')
